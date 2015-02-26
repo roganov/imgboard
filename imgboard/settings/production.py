318fdce4596e68d218bd0fa711eea5d86fa73a70
Yegor Roganov <yegor.rog@gmail.com>
2015-02-27 01:40:50 +0200

    Upload files in production on AWS S3.

--- imgboard/settings/production.py
+++ imgboard/settings/production.py
@@ -1,9 +1,23 @@
 from .base import *
+import os
 import dj_database_url
 
+INSTALLED_APPS += ('storages',)
 DATABASES = {'default': dj_database_url.config()}
 ALLOWED_HOSTS = ["*"]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+# S3 settings
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = 'imgboard-bucket-2'
+AWS_QUERYSTRING_AUTH = False
+AWS_HEADERS = {
+    'Cache-Control': 'max-age=31556926'
+}
+MEDIA_URL = 'http://imgboard-bucket-2.s3-eu-west-1.amazonaws.com/'
