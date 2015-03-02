40c57a5cfc6ce91029cf9afa9799374c33a7fff3
Yegor Roganov <yegor.rog@gmail.com>
2015-03-02 23:22:19 +0200

    Move bootstrap assets.

--- imgboard/settings/local.py
+++ imgboard/settings/local.py
@@ -1,7 +1,9 @@
 from .base import *
 
 INSTALLED_APPS += ('debug_toolbar', 'django.contrib.webdesign')
-
+# ALLOWED_HOSTS = ["*"]
+# DEBUG = False
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
